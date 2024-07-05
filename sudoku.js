07cfa6552531c52a818d02f4d71c79389ec8d118
Tamirlan <striped.pricier.0k@icloud.com>
2024-07-05 14:28:50 +0300

    pretty board func

--- sudoku.js
+++ sudoku.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const fs = require('fs');
 const os = require('os');
 
@@ -34,62 +35,50 @@ function read() {
   return result;
 }
 
+const createEmpty = (sudoku) => {
+  const empty = [];
 
-const createEmpty = sudoku => {
+  for (let i = 0; i < sudoku.length; i++) {
+    const arr = sudoku[i];
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === '-') {
+        const emp = {
+          i: i,
+          j: j,
+        };
+        const getQ = (obj, r, c) => {
+          const arr1 = [0, 1, 2];
+          const arr2 = [3, 4, 5];
+          const arr3 = [6, 7, 8];
 
-  const empty = []
-
-  for(let i = 0; i < sudoku.length; i++){
-      const arr = sudoku[i]
-      for(let j = 0; j < arr.length; j++){
-          if(arr[j] === '-'){
-              const emp = {
-                  i: i,
-                  j: j
-              }
-              const getQ = (obj, r, c) => {
-                  const arr1 = [0,1,2]
-                  const arr2 = [3,4,5]
-                  const arr3 = [6,7,8]
-
-                  if(arr1.includes(r) && arr1.includes(c)){
-                      obj.q = 1
-                  } 
-                  else if(arr1.includes(r) && arr2.includes(c) ){
-                      obj.q = 2
-                  }
-                  else if(arr1.includes(r)&& arr3.includes(c) ){
-                      obj.q = 3
-                  }
-                  else if(arr2.includes(r)&& arr1.includes(c) ){
-                      obj.q = 4
-                  }
-                  else if(arr2.includes(r)&& arr2.includes(c) ){
-                      obj.q = 5
-                  }
-                  else if(arr2.includes(r)&& arr3.includes(c) ){
-                      obj.q = 6
-                  }
-                  else if(arr3.includes(r)&& arr1.includes(c) ){
-                      obj.q = 7
-                  }
-                  else if(arr3.includes(r)&& arr2.includes(c) ){
-                      obj.q = 8
-                  }
-                  else if(arr3.includes(r)&& arr3.includes(c) ){
-                      obj.q = 9
-                  }
-               return obj
-              }
-
-              
-              empty.push(getQ(emp, i, j))
+          if (arr1.includes(r) && arr1.includes(c)) {
+            obj.q = 1;
+          } else if (arr1.includes(r) && arr2.includes(c)) {
+            obj.q = 2;
+          } else if (arr1.includes(r) && arr3.includes(c)) {
+            obj.q = 3;
+          } else if (arr2.includes(r) && arr1.includes(c)) {
+            obj.q = 4;
+          } else if (arr2.includes(r) && arr2.includes(c)) {
+            obj.q = 5;
+          } else if (arr2.includes(r) && arr3.includes(c)) {
+            obj.q = 6;
+          } else if (arr3.includes(r) && arr1.includes(c)) {
+            obj.q = 7;
+          } else if (arr3.includes(r) && arr2.includes(c)) {
+            obj.q = 8;
+          } else if (arr3.includes(r) && arr3.includes(c)) {
+            obj.q = 9;
           }
-      }
-  }
-  return empty.length !== 0 ? empty : null
-} 
+          return obj;
+        };
 
+        empty.push(getQ(emp, i, j));
+      }
+    }
+  }
+  return empty.length !== 0 ? empty : null;
+};
 
 function solve() {
   /**
@@ -105,11 +94,11 @@ function isSolved() {
    */
 }
 
-function prettyBoard() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Выводит в консоль/терминал судоку.
-   * Подумай, как симпатичнее его вывести.
-   */
-}
+function prettyBoard(arr) {
+  let result = '';
 
+  arr.forEach((element) => {
+    result += element.join(' ') + '\n';
+  });
+  return result;
+}
