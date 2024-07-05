124a968ccae1d78493f3a7316fd7f75307086fb8
Anna Shifrina <shifrina19anna@gmail.com>
2024-07-05 14:09:18 +0300

    Array of empty spaces

--- sudoku.js
+++ sudoku.js
@@ -4,6 +4,63 @@ function read() {
    */
 }
 
+const createEmpty = sudoku => {
+
+  const empty = []
+
+  for(let i = 0; i < sudoku.length; i++){
+      const arr = sudoku[i]
+      for(let j = 0; j < arr.length; j++){
+          if(arr[j] === '-'){
+              const emp = {
+                  i: i,
+                  j: j
+              }
+              const getQ = (obj, r, c) => {
+                  const arr1 = [0,1,2]
+                  const arr2 = [3,4,5]
+                  const arr3 = [6,7,8]
+
+                  if(arr1.includes(r) && arr1.includes(c)){
+                      obj.q = 1
+                  } 
+                  else if(arr1.includes(r) && arr2.includes(c) ){
+                      obj.q = 2
+                  }
+                  else if(arr1.includes(r)&& arr3.includes(c) ){
+                      obj.q = 3
+                  }
+                  else if(arr2.includes(r)&& arr1.includes(c) ){
+                      obj.q = 4
+                  }
+                  else if(arr2.includes(r)&& arr2.includes(c) ){
+                      obj.q = 5
+                  }
+                  else if(arr2.includes(r)&& arr3.includes(c) ){
+                      obj.q = 6
+                  }
+                  else if(arr3.includes(r)&& arr1.includes(c) ){
+                      obj.q = 7
+                  }
+                  else if(arr3.includes(r)&& arr2.includes(c) ){
+                      obj.q = 8
+                  }
+                  else if(arr3.includes(r)&& arr3.includes(c) ){
+                      obj.q = 9
+                  }
+               return obj
+              }
+
+              
+              empty.push(getQ(emp, i, j))
+          }
+      }
+  }
+  return empty.length !== 0 ? empty : null
+} 
+
+
+
 function solve() {
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции read.
